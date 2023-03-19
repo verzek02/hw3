59f303b632864dc6bed6293ec2e4d4c42022a1ad
verzek02 <murataalievu@gmail.com>
2023-03-19 15:25:24 +0600

    just

--- homeWorks/hw3.py
+++ homeWorks/hw3.py
@@ -1,6 +1,5 @@
 class Bank:
 
-
     def __init__(self, name, balanse):
 
         self._name = name
@@ -27,34 +26,8 @@ class Bank:
 
             return self._balanse
 
-
     def __jackpot(self):
-        return self.balanse * 10
-    @property
-    def jackpot(self):
-        return self.__jackpot()
-
-    @property
-    def name(self):
-
-        return f'{self.__name}'
-
-    @name.setter
-    def name(self, a):
-
-        self.__name = a
-
-    @property
-    def balanse(self):
-
-        return f'{self._balanse}'
-
-    @balanse.setter
-    def balanse(self, b):
-
-        self.balanse = b
-
-
+        return self._balanse * 10
 
     def _copyy(self, other):
 
@@ -73,4 +46,55 @@ print(bank._kill())
 
 bank._copyy(load)
 
-load.jackpot
+
+class Just(Bank):
+    def __init__(self, name, balanse):
+        super().__init__(name, balanse)
+
+    def name(self):
+        return self._name
+
+    def name_set(self, new_name):
+        self._name = new_name
+
+    def balance(self):
+        return self._balanse
+
+    def balance_set(self, new_balance):
+        self._balanse = new_balance
+
+
+class Just2(Bank):
+    def __init__(self, name, balanse):
+        super().__init__(name, balanse)
+
+    @property
+    def nam(self):
+        return self._name
+
+    @nam.setter
+    def nam(self, value):
+        self._name = value
+
+    @property
+    def balanc(self):
+        return self._balanse
+
+    @balanc.setter
+    def balanc(self, value):
+        self._balanse = value
+
+
+pro = Just('max', 200)
+pro2 = Just2('lida', 300)
+
+print(pro.name())
+pro.name_set('john')
+print(pro.name())
+print(pro2.nam)
+pro2.nam = 'john'
+print(pro2.nam)
+pro.balance_set(100)
+print(pro.balance())
+pro2.balanc = 122
+print(pro2.balanc)
